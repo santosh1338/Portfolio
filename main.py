22ce3bf18a3adc125a305c20f8d5e766aa73071a
Santosh <santosh.chezhyan@gmail.com>
2023-05-24 20:50:17 +0530

    Fourth commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -14,3 +15,23 @@ with col2:
     cvqkc
     """
     st.info(content)
+
+st.write("Below you can find some of the other apps that I have built. Feel free to contact me!")
+
+col3, cole, col4 = st.columns([1.5, 0.5, 1.5])
+
+data = pandas.read_csv("data.csv", sep=';')
+
+with col3:
+    for index, row in data[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row['image'])
+        st.write("[Source Code](https://youtube.com)")
+
+with col4:
+    for index, row in data[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row['image'])
+        st.write("[Source Code](https://youtube.com)")
